5f4d0d6113c3c105421aaef11b3d6f86d3d4b9f4
Charlie Heiner <heinercj@gmail.com>
2018-07-25 12:19:32 -0700

    refactored tests

--- test/Fridge.test.js
+++ test/Fridge.test.js
@@ -1,28 +1,32 @@
 const assert = require('assert');
 const Fridge = require('../Fridge');
 
-describe('smart fridge class tests', () => {
+describe('smart fridge default test', () => {
+  
     it('smart fridge inventory defaults to empty {}', () => {
         const fridge = new Fridge();
         assert.deepEqual(fridge.inventory, {});
     });
+});
+
+describe('smart fridge class tests', () => {
+    let fridge = null;
+    
+    beforeEach(function(){
+        fridge = new Fridge();
+        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
+    });
 
     it('adds item to fridge', () => {
-        const fridge = new Fridge();
-        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
         assert.deepEqual(fridge.inventory, { 1234: { 'itemType': 'dairy', 'name': 'milk', 'fillFactor': 1, 'in': true } });
     });
 
     it('it adds to item total if item is already in fridge', () => {
-        const fridge = new Fridge();
-        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
         fridge.handleItemAdded('dairy', 1234, 'milk', 1);
         assert.deepEqual(fridge.inventory, { 1234: { 'itemType': 'dairy', 'name': 'milk', 'fillFactor': 2, 'in': true } });
     });
 
     it('event handler removes item in property to false. subtracts fillFactor', () => {
-        const fridge = new Fridge();
-        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
         fridge.handleItemRemoved(1234);
         assert.deepEqual(fridge.inventory, { 1234: { 'itemType': 'dairy', 'name': 'milk', 'fillFactor': 0, 'in': false } });
 
@@ -31,8 +35,6 @@ describe('smart fridge class tests', () => {
     });
 
     it('getItems query returns item of fillFactor less than equal to requested', () => {
-        const fridge = new Fridge();
-        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
         fridge.handleItemAdded('juice', 5678, 'orange juice', .4);
         fridge.handleItemAdded('fruit', 2468, 'apples', .3);
         const list = fridge.getItems(.5);
