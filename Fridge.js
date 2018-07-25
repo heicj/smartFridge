a39ae6a8d10cc4e1a2f0902318dc206c385766ad
Charlie Heiner <heinercj@gmail.com>
2018-07-25 11:28:52 -0700

    adds item to fridge.  adds to fill factor if present

--- Fridge.js
+++ Fridge.js
@@ -2,6 +2,14 @@ class SmartFridgeManager {
     constructor(){
         this.inventory = {};
     }
+
+    handleItemAdded(itemType, itemUUID, name, fillFactor){
+        if(this.inventory[itemUUID]){
+            this.inventory[itemUUID].fillFactor += fillFactor;
+        } else {
+            this.inventory[itemUUID] = {itemType: itemType, name: name, fillFactor: fillFactor };
+        }
+    }
 }
 
 module.exports = SmartFridgeManager;
--- test/Fridge.test.js
+++ test/Fridge.test.js
@@ -6,4 +6,17 @@ describe('smart fridge class tests', () => {
         const fridge = new Fridge();
         assert.deepEqual(fridge.inventory, {});
     });
+
+    it('adds item to fridge', () => {
+        const fridge = new Fridge();
+        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
+        assert.deepEqual(fridge.inventory, { 1234: { 'itemType': 'dairy', 'name': 'milk', 'fillFactor': 1 } });
+    });
+
+    it('it adds to item total if item is already in fridge', () => {
+        const fridge = new Fridge();
+        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
+        fridge.handleItemAdded('dairy', 1234, 'milk', 1);
+        assert.deepEqual(fridge.inventory, { 1234: { 'itemType': 'dairy', 'name': 'milk', 'fillFactor': 2 } });
+    });
 });
